74728105ae3bf783cdd5022c223f95eaae7a5314
Турик Александр Геннадьевич <a.turik@corp.upp.com.ru>
2022-01-26 14:15:43 +0300

    добавил массив с названиями тасков

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const addNewTask = document.querySelector('.todo-form__button')// кнопка добавления новой задачи
+  todo();
+});
+
+function todo(){
+  const addNewTaskBtn = document.querySelector('.todo-form__button')// кнопка добавления новой задачи
   const newTaskName = document.querySelector('.todo-form__new-task-name')// имя новой задачи
   const taskList = document.querySelector('.todo')// список заданий
+  const taskDB = [] // ключ - название таска, значение - name input
 
-  const createNewNask = () => {
+  const createNewTask = () => {
     if(newTaskName.value) { 
       const taskWrapper = document.createElement('div')
-            taskWrapper.classList = 'todo__wrapper';
+      taskWrapper.classList = 'todo__wrapper';
+
       const taskCheck = document.createElement('input')
-            taskCheck.type = 'checkbox'
-            taskCheck.classList = 'task-ckeck'
-            taskCheck.name = ''
+      taskCheck.type = 'checkbox'
+      taskCheck.classList = 'task-ckeck'
+      taskCheck.name = ''
+
       const newTask = document.createElement('li');
-            newTask.classList = 'todo__item';
-            newTask.innerText = newTaskName.value;
+      newTask.classList = 'todo__item';
+      newTask.innerText = newTaskName.value;
       
       taskList.append(taskWrapper)
       taskWrapper.append(taskCheck)
@@ -21,8 +28,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
   
-  addNewTask.addEventListener('click', () => {
-    createNewNask()
+  addNewTaskBtn.addEventListener('click', () => {
+    createNewTask()
+    taskDB.push({[newTaskName.value] : newTaskName.name})
   })
-
-});
+}
