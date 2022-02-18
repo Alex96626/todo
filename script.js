f6763ac1c541a73e66614f1aee90df4dbfe49bcc
Турик Александр Геннадьевич <a.turik@corp.upp.com.ru>
2022-02-18 18:34:36 +0300

    туда сюда правка

--- script.js
+++ script.js
@@ -5,13 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
 function todo(){
   const formTodo = document.getElementById('todo1')
   const newTaskName = document.querySelector('.todo-form__new-task-name')// имя новой задачи
-  const taskList = document.querySelector('.todo')// список заданий  
+  const taskListOnBoard = document.querySelector('.todo')// список заданий  
   const taskDB = [] // ключ - название таска, значение - name input
 
-  const createNewTask = (index, value, checked) => {
+  
+
+  const createNewTask = ({index, value, checked}) => {
+
     const getNewTask = document.createElement('li')
     getNewTask.classList = 'todo__item'
     const getTaskCheck = document.createElement('input')
+    
     getTaskCheck.classList ="task-check"
     getTaskCheck.name = `task-name_${index}`
     getTaskCheck.type = 'checkbox'
@@ -23,17 +27,17 @@ function todo(){
     getNewTask.append(getTaskCheck)
     getNewTask.append(getTaskValue)
 
-    return getNewTask;
+    return getNewTask // созданая НОД(li)
   }
 
-  const addNewTask  = (newTaskIndex, newTaskValue, checked) => {
-    return taskList.append(createNewTask(newTaskIndex, newTaskValue, checked))
+  const appendTasks  = (taskList) => {
+    taskListOnBoard.append(...taskList.map( (item) => {return createNewTask(item)}))
   }
 
-  const saveStateTask = (index, value, checked) => {
+  const saveStateTask = ({index, value, checked}) => {
     return taskDB.push({
       index: index,
-      [newTaskName.name] : value,
+      value : value,
       checked: checked
     })
   }
@@ -41,37 +45,54 @@ function todo(){
   const checkTaskStatus = (event) => {
       const target = event.target
       const taskInfo = target.closest('.todo__item');// li-шка
+
       if(!taskInfo.contains(taskInfo.querySelector('.task-check'))) return;
       if(target !== taskInfo.querySelector('.task-check')) return;
+      
       const taskIndex = target.name.split('_')[1]// получили индекс такса
       taskDB[Number(taskIndex) - 1].checked = target.checked;
+      
       saveLocalStorage(taskIndex)
-      console.log(taskDB)
   }
 
-  const saveLocalStorage = (newTaskIndex) => {
-    localStorage.setItem(String(newTaskIndex), JSON.stringify(taskDB[newTaskIndex - 1]))
+  const saveLocalStorage = () => {
+    localStorage.setItem('taskList', JSON.stringify(taskDB))
   }
 
-  (function loadFromLocalStorage () {
-    const getLocalStorageInfo = Object.values(localStorage)
-    getLocalStorageInfo.map( (e)=> {
-      const index = JSON.parse(e).index
-      const value = JSON.parse(e).input
-      const checked  = JSON.parse(e).checked
+  const loadFromLocalStorage = () => {
+    const getLocalStorageInfo = JSON.parse(localStorage.getItem('taskList'))
 
-      saveStateTask(index, value, checked)
-      createNewTask(index, value, checked)
-      addNewTask(index, value, checked)
-    });
-  }())
+    if(getLocalStorageInfo){
+    getLocalStorageInfo.forEach((e) => {
+      
+      const localStorageTaskInfo = {
+        index : e.index,
+        value : e.value, 
+        checked : e.checked
+      }
+      saveStateTask(localStorageTaskInfo)
+      
+    })
+
+      saveLocalStorage()
+      appendTasks(taskDB)
+
+    }
+  }
+  
+  loadFromLocalStorage()
   
   formTodo.addEventListener('submit', (event) => {
-    const newTaskValue = newTaskName.value
-    const newTaskIndex = taskDB.length + 1;
-    event.preventDefault();
-    addNewTask(newTaskIndex, newTaskValue, false, saveStateTask(newTaskIndex, newTaskValue, false ))
-    saveLocalStorage(newTaskIndex)
+    event.preventDefault()
+    const taskInfo = {
+      index : taskDB.length + 1,
+      value : newTaskName.value,
+      checked : false,
+    }
+    saveStateTask(taskInfo )
+    appendTasks([taskInfo])
+
+    saveLocalStorage()
     
   })
 }
