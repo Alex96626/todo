81df423c60cea754bead1025be06784c80cda881
Турик Александр Геннадьевич <a.turik@corp.upp.com.ru>
2022-02-22 17:45:54 +0300

    переделал Data

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ function todo(){
   const newTaskDescription = document.querySelector('.todo-form__task-description')
   const taskListOnBoard = document.querySelector('.todo')// список заданий  
   const tasksList = JSON.parse(localStorage.getItem('taskList')) ?? []/// ключ - название таска, значение - name input
+  
 
-  const createNewTask = ({index, value, checked, description}) => {
+  const createNewTask = ({index, value, checked, description, date}) => {
 
     const getNewTask = document.createElement('li')
     getNewTask.classList = 'todo__item'
@@ -27,15 +28,14 @@ function todo(){
 
     const getTaskDescription = document.createElement('p')
     getTaskDescription.classList = 'task-description'
-    getTaskDescription.innerText = description
+    getTaskDescription.innerText = description ?? ''
 
     const getTaskDate = document.createElement('p')
     getTaskDate.classList = 'task-date'
-    const nowDay = new Date().getDate() < 10 ? '0' + new Date().getDate() : new Date().getDate()
-    const nowMonth = new Date().getMonth() < 10 ? '0' + new Date().getMonth() : new Date().getMonth()
-    const nowYear = new Date().getFullYear()
-    getTaskDate.innerText = nowDay + '.' + nowMonth+ '.' + nowYear;
-
+    // const nowDay = new Date().getDate() < 10 ? '0' + new Date().getDate() : new Date().getDate()
+    // const nowMonth = new Date().getMonth() < 10 ? '0' + new Date().getMonth() : new Date().getMonth()
+    // const nowYear = new Date().getFullYear()
+    getTaskDate.innerText = date
     getNewTask.append(getTaskCheck)
     getNewTask.append(getTaskValue)
     getNewTask.append(getTaskDescription)
@@ -49,12 +49,13 @@ function todo(){
     taskListOnBoard.append(...taskList.map( createNewTask ))
   }
 
-  const addTaskInList = ({index, value, checked, description}) => {
+  const addTaskInList = ({index, value, checked, description, date}) => {
     tasksList.push ({
       index: index,
       value : value,
       description : description,
-      checked: checked
+      checked: checked,
+      date:date,
     })
   }
 
@@ -83,13 +84,17 @@ function todo(){
   
   formTodo.addEventListener('submit', (event) => {
     event.preventDefault()
-
+    const nowDay = new Date().getDate()
+    const nowMonth = new Date().getMonth()
+    const nowYear = new Date().getFullYear()
     const taskInfo = {
       index : tasksList.length + 1,
       value : newTaskName.value,
       description : newTaskDescription.value,
       checked : false,
+      date : (nowDay < 10 ? '0' + nowDay : nowDay) + '.' + (nowMonth < 10 ? '0' + nowMonth : nowMonth) + '.' + (nowYear),
     }
+
 
     addTaskInList(taskInfo )
     appendTasks([taskInfo])
